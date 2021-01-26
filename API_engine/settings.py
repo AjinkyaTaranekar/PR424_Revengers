ec7f68d00e3722103ffa0c699a041235d71e3d52
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2021-01-26 11:21:56 +0530

    port change

--- API_engine/settings.py
+++ API_engine/settings.py
@@ -24,7 +24,7 @@ class APISettings(BaseSettings):
 
 
 class MongoSettings(BaseSettings):
-    uri: str = "mongodb://127.0.0.1:27017"
+    uri: str = "mongodb://127.0.0.1:5001"
     database: str = "robot_banao"
     users: str = "users"
 
